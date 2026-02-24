6dba3dfaf0cbe7c121c2b0a2dfc7fc770ac459be
Mevinu Abeysinghe <m.mevinuabeysingha@gmail.com>
2026-02-24 13:48:02 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -26,4 +26,17 @@ def menu():
         choices=["Prefix", "Suffix", "Replace Text", "Auto Numbering"]).ask()
     return rename_option
 
-
+def do_rename(rename_option, folder_path):
+    if rename_option == "Prefix":
+        prefix = questionary.text("Enter the prefix to add:").ask()
+        
+    elif rename_option == "Suffix":
+        suffix = questionary.text("Enter the suffix to add:").ask()
+        
+    elif rename_option == "Replace Text":
+        old_text = questionary.text("Enter the text to replace:").ask()
+        new_text = questionary.text("Enter the new text:").ask()
+        
+    elif rename_option == "Auto Numbering":
+        
+        pass
