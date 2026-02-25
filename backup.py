49f2e79ec7334601a857d2b116de72e404bdca9a
Mevinu Abeysinghe <m.mevinuabeysingha@gmail.com>
2026-02-25 11:11:26 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -3,6 +3,8 @@
 #Timestamped Backup Folder
 #Copy All Files and Subfolders (Recursive)
 #Summary Report
+#Only copy: New files,Modified files
+#Compare: File size, Last modified time, OR file hash (advanced)
 
 def main(path):
     print(f"path to folder backup {path}")
