d4f127b75369e5c26799d07265b56daaa2f1af56
Mevinu Abeysinghe <m.mevinuabeysingha@gmail.com>
2026-02-25 11:12:07 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -5,6 +5,9 @@
 #Summary Report
 #Only copy: New files,Modified files
 #Compare: File size, Last modified time, OR file hash (advanced)
+#Skip Unchanged Files
+#File Type Exclusion
+#Preview Mode
 
 def main(path):
     print(f"path to folder backup {path}")
