4e36e7295d09b7bc0d8cc6e8bfa483860ba08b1f
Mevinu Abeysinghe <m.mevinuabeysingha@gmail.com>
2026-02-25 11:09:30 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,6 +1,8 @@
 #Select Source Folder
 #Select Destination Folder
 #Timestamped Backup Folder
+#Copy All Files and Subfolders (Recursive)
+#Summary Report
 
 def main(path):
     print(f"path to folder backup {path}")
