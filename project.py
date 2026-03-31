40e6e613a4ca65fd57b2d9b1eba1b9dd0a3bee40
Mevinu Abeysinghe <m.mevinuabeysingha@gmail.com>
2026-03-31 20:34:37 +0530

    added comments

--- project.py
+++ project.py
@@ -1,26 +1,30 @@
 # File Manager (FileMgr) By Mevinu Abeysinghe
 # For CS50 Python Final Project
 import argparse
-import os
 import sys
 from pathlib import Path
-import questionary  # type: ignore
+import questionary
 import menu
 import backup
 import organize
 import rename
 
+# main function to run the program
 def main():
     try:
         source_path, action = parse_arguments()
-        if not check_path(source_path):
+
+        if not check_path(source_path): # if path is not provided or invalid, ask for path
             source_path = ask_for_path()
+
         selected_action = check_action(action)
         do_action(selected_action,source_path)
+
     except Exception as e:
         print(f"error {e}")
         sys.exit(1)
 
+# function to parse command-line arguments
 def parse_arguments():
     # setup command-line arguments
     parser = argparse.ArgumentParser(description="FileMgr")
@@ -31,13 +35,23 @@ def parse_arguments():
     path = args.path
     source_path = Path(path) if path else None
     action = args.action
-    return source_path, action
+    return source_path, action # returns the path & action
 
+# function to check if path is valid and exists
+def check_path(source_path):
+    if source_path is None:
+        return False
+    if source_path.exists() and source_path.is_dir():  # cheking if path is available and its directory
+        return True
+    else:
+        return False
+
+# function to ask user for path if not provided in arguments
 def ask_for_path():
     while True:
         path = questionary.text("Enter the source folder path:").ask()
 
-        if path is None:
+        if path is None: # if user cancels the input (control+c) exit from program
             print("Exiting FileMgr")
             sys.exit(0)
 
@@ -47,22 +61,7 @@ def ask_for_path():
         else:
             print("Invalid path. Please enter a valid folder path.")
 
-def check_path(source_path):
-    if source_path is None:
-        return False
-    if source_path.exists() and source_path.is_dir():  # cheking if path is available
-        return True
-    else:
-        return False
-
-def ask_for_action():
-    action = questionary.select("Select Action:", choices=[
-                                "backup", "rename", "organize"]).ask()
-    if action is None:
-        print("Exiting FileMgr")
-        sys.exit(0)
-    return action
-
+# function to check if action is provided, if not ask for action
 def check_action(action):
     if not action:
         print("FileMgr")
@@ -71,10 +70,23 @@ def check_action(action):
     else:
         return action
 
+# function to ask user for action if not provided in arguments
+def ask_for_action():
+    action = questionary.select("Select Action:", choices=[
+                                "backup", "rename", "organize"]).ask()
+    if action is None:
+        print("Exiting FileMgr")
+        sys.exit(0)
+    return action
+
+# function to do the selected action
 def do_action(action, source_path):
     if action == "backup":
+         # checks if quick backup is avalavle and user wants to do it
         if backup.is_quick_backup(source_path) and menu.ask_quick_backup():
                 backup.do_quick_backup(source_path)
+
+        # if quick backup is not available/user do not want to do it, show backup menu
         else:
             usr_config = menu.backup_menu(source_path)
             if usr_config is None:
